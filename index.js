d77b7c140b2d2ffe052088bcb9089f3ebea1ecd4
WhoopeeGoldberg <40154126+WhoopeeGoldberg@users.noreply.github.com>
2018-06-14 19:23:36 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,226 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+bot.on('message', (message) => {
+
+if(message.content == 'ping') {
+    message.reply('pong');
+}
+
+if(message.content == 'Oof') {
+    message.reply('I will never love Maul');
+}
+
+if(message.content == 'oof') {
+    message.reply('I will never love Maul');
+}
+
+if(message.content == 'Fuck') {
+    message.reply('Harder daddy');
+}
+
+if(message.content == 'E') {
+    message.reply('E');
+}
+
+if(message.content == 'Rip') {
+    message.reply('Just like Mauls chances with me');
+}
+
+if(message.content == 'Jared') {
+    message.reply('Thats my boyfriends name!');
+}
+
+if(message.content == 'Maul') {
+    message.reply('Alex is the biggest pussy he will never ask me out');
+}
+
+if(message.content == 'Garrett') {
+    message.reply('Oh his is bigger than Alex');
+}
+
+if(message.content == 'Gay') {
+    message.reply('Maul is my gay best friend! Hes like a brother!');
+}
+
+if(message.content == 'no u') {
+    message.reply('used Uno reverse card, just like Maul did with his sexuality!');
+}
+
+if(message.content == 'Hello there') {
+    message.reply('General Kenobi, youre bolder than Maul!');
+}
+
+if(message.content == '...') {
+    message.reply('Dont DM me please.');
+}
+
+if(message.content == 'I am the senate') {
+    message.reply('Not yet Jared.');
+}
+
+if(message.content == '..') {
+    message.reply('Dont DM me please.');
+}
+
+if(message.content == 'Mollie') {
+    message.reply('That prostitute?!');
+}
+
+
+
+if(message.content == 'No balls') {
+    message.reply('Just like Maul and Grievous');
+}
+
+if(message.content == 'Nien') {
+    message.reply('What a thicc boi');
+}
+
+if(message.content == 'yeet') {
+    message.reply('yoot');
+}
+
+if(message.content == 'Not yet') {
+    message.reply('Lets just be friends');
+}
+
+if(message.content == 'Yeet') {
+    message.reply('Yoot');
+}
+
+if(message.content == 'Luke') {
+    message.reply('With a snap of my fingers, your discord token will cease to exist.');
+}
+
+if(message.content == 'See you around kid.') {
+    message.reply('Are you some gay roleplayer?');
+}
+
+if(message.content == 'Diego') {
+    message.reply('Lego Marvel IS my life mom');
+}
+
+if(message.content == 'Ashton') {
+    message.reply('Were breaking up. I know you cheated with Alyssa.');
+}
+
+if(message.content == 'Malgus') {
+    message.reply('Jon lookin fine...');
+}
+
+if(message.content == 'Admin abuse') {
+    message.reply('Right back at ya');
+}
+
+if(message.content == 'Nicole') {
+    message.reply('Hi! I am Nicole, Maul wishes I was his, but it will never happen haha');
+}
+
+if(message.content == 'Fordo') {
+    message.reply('How many dicks did he suck for admin? The world may never know.');
+}
+
+if(message.content == 'Bacara') {
+    message.reply('He has a secret crush on Mollie shhhhh');
+}
+
+if(message.content == 'Admin abuse') {
+    message.reply('Right back at ya');
+}
+
+if(message.content == 'GP') {
+    message.reply('Stop inflating the economy');
+}
+
+if(message.content == 'Tup') {
+    message.reply('You know I had to Tup it to em.');
+}
+
+if(message.content == 'Snoke') {
+    message.reply('Mr. Melatonin, I dont feel so good');
+}
+
+if(message.content == 'Ben') {
+    message.reply('*Bomb Voyage*');
+}
+
+if(message.content == 'Rako') {
+    message.reply('What a gay ass weeb');
+}
+
+if(message.content == 'Han') {
+    message.reply('Why are you trying to talk to him? He isnt active.');
+}
+
+if(message.content == 'Omae Wa Mou Shindeiru') {
+    message.reply('*Nani?!*');
+}
+
+if(message.content == 'Begone Thot') {
+    message.reply('No u');
+}
+
+if(message.content == 'Begone thot') {
+    message.reply('No u');
+}
+
+if(message.content == 'Mister') {
+    message.reply('Retsim');
+}
+
+if(message.content == 'Retsim') {
+    message.reply('Mister');
+}
+
+if(message.content == 'No.') {
+    message.reply('Hm.');
+}
+
+
+if(message.content == 'Sith Cafe') {
+    message.reply('You fought in the Cafe Wars?');
+}
+
+if(message.content == 'Alliance') {
+    message.reply('RIP');
+}
+
+if(message.content == 'Chirrut') {
+    message.reply('The only Alliance member with a gf');
+}
+
+if(message.content == '!skip') {
+    message.reply('Dont pussy out');
+}
+
+if(message.content == 'Ahsoka') {
+    message.reply('RACHEL? WHERE IS SHE?!?!');
+}
+
+if(message.content == '!play') {
+    message.reply('Uh I have a bf');
+}
+
+if(message.content == 'This is where the fun begins') {
+    message.reply('Uh how about no');
+}
+
+if(message.content == 'Thicc') {
+    message.reply('Only tolerable ;)');
+}
+
+if(message.content == 'Do or do not there is no try') {
+    message.reply('Size matters not. Jk it does lmao');
+}
+
+if(message.content == 'Jango') {
+    message.reply('.......');
+}
+
+if(message.content == 'Sophie') {
+    message.reply('Lol that ugly thot');
+}
+
+});
+
+bot.login('NDU1NDI1NjA4MjI3MjI1NjEw.Df8Gvg.8fNv4PP-cqF9pk0PbDQnI6574Vg');
